ed526a4713b7963259597ff5b5dc3c3c3a2aa017
Tim-K3007 <oitsplazma444@gmail.com>
2023-02-14 19:04:12 -0500

    made serial com more robust

--- python_scripts/com.py
+++ python_scripts/com.py
@@ -3,23 +3,23 @@ import serial.tools.list_ports
 
 
 def serial_com(direction):
-    mega_id = "/dev/ttyACM0"
+    mega_id = "85138313134351709030"
     port = "not found"
 
     list_of_ports = serial.tools.list_ports.comports()
 
     for portInfo in list_of_ports:
-        if portInfo.device == mega_id:
+        if portInfo.serial_number == mega_id:
             port = portInfo.device
             break
-        # print(portInfo.device)
+        # print(portInfo.serial_number)
 
     if port != "not found":
         ser_port = serial.Serial(port)
         ser_port.write(direction)
         ser_port.close()
     else:
-        print("Make sure the right ESP32 is plugged in and re-run")
+        print("Make sure the right Mega is plugged in and re-run")
         exit(-1)
 
 
