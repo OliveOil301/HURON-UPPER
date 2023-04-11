f29937f5183a41e6ae4be98b46f3ee578f4d270f
Jacob <jbernard@wpi.edu>
2023-04-10 22:35:49 -0400

    Did more work on the command queue stuff. Also stgarted fleshing out the main loop and switch case. Currently, we cna just leave the idel state. None of the other states have been completed

--- Arduino Mega Torso Control/src/main.cpp
+++ Arduino Mega Torso Control/src/main.cpp
@@ -139,15 +139,20 @@ enum Command{
  * 'G' - GET, get data on the current position
 */
 
-int commandDigits[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-//This array holds the digits of the most recent command. If there is no command, it should be all 0
 
 unsigned long currentTime = 0;
 
-int actuatorPositionGoals[10][4];//the goal position of the actuators, in millimeters
-int currentPositionGoalIndex = 0; //The index of the above array for the current position goal.
-int newPositionGoalIndex = 1; // This stores the index for the next goal to be set to. This is used to keep track of the new goals in the system 
-//TODO: Add a goal in setup() that adds a goal for the home position.
+//* QUEUE CODE ----------------------
+int commandQueue[10][5];//the queue of commands
+// The command queue is structured as follows:
+// |   num1   |   num2   |   num3   |   num4   |   Command Char   |
+// This is structured to make movements as easy to understand as possible but 
+// also allows other commands to sit in the queue, like D (DELAY) and G (GET) commands
+
+int currentCommandIndex = 0; //The index of the above array for the current position goal.
+int commandInsertIndex = 1; // This stores the index for the next command to be set to. This is used to keep track of the new goals in the system 
+
+
 
 /** void clearSerialUntilCommand()
  * clears each character until a valid command start character is recieved.
@@ -174,20 +179,20 @@ void printDebugToScreen(String line){
 }
 
 void printActuatorGoals(){
-  // Only works on newPositionGoalIndex from 1-9, not 0
+  // Only works on commandInsertIndex from 1-9, not 0
   // Can be fixed but would make it a bit more complicated. 
-  // Not worth it since this is jsut for testing
+  // Not worth it since this is just for testing
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(10, 0);
-  display.print(String(actuatorPositionGoals[newPositionGoalIndex-1][0]));
+  display.print(String(commandQueue[commandInsertIndex-1][0]));
   display.print(", ");
-  display.print(String(actuatorPositionGoals[newPositionGoalIndex-1][1]));
+  display.print(String(commandQueue[commandInsertIndex-1][1]));
   display.print(", ");
-  display.print(String(actuatorPositionGoals[newPositionGoalIndex-1][2]));
+  display.print(String(commandQueue[commandInsertIndex-1][2]));
   display.print(", ");
-  display.print(String(actuatorPositionGoals[newPositionGoalIndex-1][3]));
+  display.print(String(commandQueue[commandInsertIndex-1][3]));
   display.display(); 
 
 }
@@ -196,10 +201,10 @@ void printActuatorGoals(){
  * sets the first position goal to be the home position of the robot
 */
 void setHomePositionGoal(){
-  actuatorPositionGoals[0][0] = 276;
-  actuatorPositionGoals[0][1] = 276;
-  actuatorPositionGoals[0][2] = 266;
-  actuatorPositionGoals[0][3] = 266;
+  commandQueue[0][0] = 276;
+  commandQueue[0][1] = 276;
+  commandQueue[0][2] = 266;
+  commandQueue[0][3] = 266;
 }
 
 /** bool readCommand()
@@ -207,64 +212,59 @@ void setHomePositionGoal(){
  * If a command was recieved, the command type is stored in the global variable "currentCommand"
  * If a move command was recieved, the actuatorGoalPosition list is updated with the requested position
 */
-Command readCommand()
+bool readCommand()
 {
   if (Serial.available() < 13){
-    //if there are less than 13 characters in the serial port
-    //return NONE since there isn't a command or isn't one yet
+    //IF: there aren't enough characters in the port for a command to be read,
+    //   - return NONE since there isn't a command or isn't one yet
     return NONE;
   } else {
-    printDebugToScreen("Command Recieved (117)");
-
-    //If we're here, there are at least 13 characters in the serial bus
-    //we now need to check if the first one is a valid command character (M or G)
+    //IF: there *are* enough characters in the port for a command to be read,
+    
     char firstChar = Serial.read();
     if (firstChar != 'G' && firstChar != 'M'){
-      printDebugToScreen("No valid st char (123)");
-      printDebugToScreen(String(firstChar));
-      //If it's not a valid command start character,
-      //clear the serial bus until a valid start character is found
-      //then return false and the next loop can try to read the command
+      //IF: the first character is *not* a valid command start character,
+      //   - clear the serial bus until a valid start character is found
+      //   - then return false and the next loop can try to read the command
       clearSerialUntilCommand();
       return NONE;
     } else {
-      printDebugToScreen("Valid st char (130)");
-
+      //IF: the first character *is* a valid command start character,
+      //   - Read the characters and convert them to 12 numbers instead of 12 number chars
+      //   - Call addGoalToQueue() which will handle higher-level error checking on commands
+      //   - return 
       //At this point, we have enough characters and it's a valid 
       //command start character at the beginning
       //We'll read through the chars and add them to the global array before returning the 
       //command type
       
-      //Read the 12 command digits
+      //Read the 12 command digits and convert them to the actual number (char '2' -> int 2)
+      int commandDigits[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
       for (int i = 0; i<12; i++){
         commandDigits[i] = Serial.read() - 48;
       }
 
-      if (firstChar == 'M'){
-        //Serial.println("MOVE");
-        return MOVE;
-      } else if (firstChar == 'G'){
-        //Serial.println("GET");
-        return GET;
-      } else {
-        return NONE; //This shouldn't happen but it's here for safety
-      }
+
+      //RETURN the output of addGoalToQueue
+      //   - True if the command was added to the queue, false for the following reasons:
+      //     - False if a move command is out of the bounds of any actuator
+      //     - False if the queue is full
+      return addGoalToQueue(firstChar, commandDigits);
     }
   }
 }
 
-/** bool addMoveGoal(int act1, int act2, int act3, int act4)
- * @param act1: an integer representing the goal for the first actuator
- * @param act2: an integer representing the goal for the second actuator
- * @param act3: an integer representing the goal for the third actuator
- * @param act4: an integer representing the goal for the fourth actuator
+/** bool addGoalToQueue(int commandChar, int* commandNumbers)
+ * @param commandChar: the char of the command start to be saved with the numbers
+ * @param commandNumbers: a list of integers representing the actions for a given command
  * @returns: a boolean representing whether the goal was added to the list. 
  *      If there are already 10 goals that have not yet been completed, the
  *      goal cannot be added to the list and false will be returned. 
  *      Otherwise, true is returned
+ *      false will also be returned if there was an error with the command (position isn't valid, etc.)
 */
-bool addMoveGoal(int* commandNumbers){
-  if(newPositionGoalIndex == currentPositionGoalIndex){
+bool addGoalToQueue(int commandChar, int* commandNumbers){
+  if(commandInsertIndex == currentCommandIndex){
     //If we will overwrite the oldest goal, return false and disregard the movement goal
     return false;
   } else { //If we are good to add the goal to the array:
@@ -277,15 +277,27 @@ bool addMoveGoal(int* commandNumbers){
     }
 
     //add the new goal to the correct position in the array
-    actuatorPositionGoals[newPositionGoalIndex][0] = actuatorPositions[0];
-    actuatorPositionGoals[newPositionGoalIndex][1] = actuatorPositions[1];
-    actuatorPositionGoals[newPositionGoalIndex][2] = actuatorPositions[2];
-    actuatorPositionGoals[newPositionGoalIndex][3] = actuatorPositions[3];
+    commandQueue[commandInsertIndex][0] = actuatorPositions[0];
+    commandQueue[commandInsertIndex][1] = actuatorPositions[1];
+    commandQueue[commandInsertIndex][2] = actuatorPositions[2];
+    commandQueue[commandInsertIndex][3] = actuatorPositions[3];
+    commandQueue[commandInsertIndex][4] = commandChar;//Saving the type of command
 
     //index the position while keepping it constrained from 0 to 9 with modulo
-    newPositionGoalIndex = (newPositionGoalIndex+1)%10;
+    commandInsertIndex = (commandInsertIndex+1)%10;
     
     return true;// Now return true since everything worked out correctly
+  }
+}
+
+/** bool newCommandsAvailable()
+ * @return true if there is a new command in the queu available to complete
+*/
+bool newCommandsAvailable(){
+  if(currentCommandIndex == commandInsertIndex){//If the next goal has not been filled yet
+    return false;
+  } else {
+    return true;
   }
 }
 
@@ -333,6 +345,13 @@ void setup()
   printDebugToScreen("Setup Complete");
 }
 
+enum State{
+  IDLE,
+  COMMAND_MOVING,
+  COMMAND_WAITING,
+  COMMAND_GETTING
+};
+State currentState = IDLE;
 
 
 void loop()
@@ -343,14 +362,55 @@ void loop()
    * If it was a GET command, send whatever information was requested
    * 
   */
-  Command tempComm = readCommand();
+  bool commandReceived = readCommand();
   currentTime = millis();
+
+  switch(currentState){
+    case IDLE:
+      //If we are in this state, all of the commands in the queue have been completed, or we just finished a command
+      //To get out, any of the following needs to be true
+      //   - We are behind a command (one was added) 
+
+      if (newCommandsAvailable()){
+        //If we have a new command, find what it is and move to the corresponding state
+        switch(commandQueue[currentCommandIndex][4]){
+          case 'M':
+            //We have a MOVE command up next:
+
+            //save the current actuator positions
+
+            //switch to the moving state
+            currentState = COMMAND_MOVING;
+            break;
+          case 'W':
+            //We have a WAIT command up next:
+            currentState = COMMAND_WAITING;
+            break;
+          case 'G':
+            //We have a GET command up next:
+            currentState = COMMAND_GETTING;
+            break;
+        }
+      }
+      break;
+    case COMMAND_MOVING:
+      //We have to move with a move command
+      break;
+    case COMMAND_WAITING:
+      //stuff
+      break;
+    case COMMAND_GETTING:
+      //stuff
+      break;
+    
+  }
+
 
   if(tempComm == NONE){//If there isn't a full command or not a full command yet
   //Do nothing
   } else if(tempComm == MOVE){//If there was just a MOVE command read
     printDebugToScreen("MOVE");
-    if(addMoveGoal(commandDigits)){//Add it to the queue, if possible
+    if(addGoalToQueue(commandDigits)){//Add it to the queue, if possible
       printActuatorGoals();
     } else {
       printDebugToScreen("MOVE Queue full");
